c31b1154ed0b52c77ff703069b396ee34edc52bc
Hila Mefano <hila@ayende.com>
2015-08-12 17:18:12 +0300

    compile errors fixed

--- Raven.Database/Config/MemoryStatistics.cs
+++ Raven.Database/Config/MemoryStatistics.cs
@@ -23,9 +23,9 @@ namespace Raven.Database.Config
 		LowMemoryHandlerStatistics GetStats();
 	}
 
-    internal static class MemoryStatistics
-    {
-        private static readonly ILog Log = LogManager.GetCurrentClassLogger();
+	internal static class MemoryStatistics
+	{
+		private static readonly ILog Log = LogManager.GetCurrentClassLogger();
 
 		private const int LowMemoryResourceNotification = 0;
 
@@ -44,20 +44,27 @@ namespace Raven.Database.Config
 		[DllImport("Kernel32.dll", SetLastError = true)]
 		public static extern bool SetEvent(IntPtr hEvent);
 
-        private static bool failedToGetAvailablePhysicalMemory;
-        private static bool failedToGetTotalPhysicalMemory;
-        private static int memoryLimit;
-        private static readonly IntPtr LowMemoryNotificationHandle;
-        private static readonly ConcurrentSet<WeakReference<ILowMemoryHandler>> LowMemoryHandlers = new ConcurrentSet<WeakReference<ILowMemoryHandler>>();
-        private static readonly IntPtr LowMemorySimulationEvent = CreateEvent(IntPtr.Zero, false, false, null);
+		private static bool failedToGetAvailablePhysicalMemory;
+		private static bool failedToGetTotalPhysicalMemory;
+		private static int memoryLimit;
+		private static readonly IntPtr LowMemoryNotificationHandle;
+		private static readonly ConcurrentSet<WeakReference<ILowMemoryHandler>> LowMemoryHandlers = new ConcurrentSet<WeakReference<ILowMemoryHandler>>();
+		private static readonly IntPtr LowMemorySimulationEvent = CreateEvent(IntPtr.Zero, false, false, null);
 		private static readonly IntPtr SoftMemoryReleaseEvent = CreateEvent(IntPtr.Zero, false, false, null);
 
-		private static ManualResetEvent stopPosixLowMemThreadEvent = new ManualResetEvent(false);
-		private static ManualResetEvent lowPosixMemorySimulationEvent = new ManualResetEvent(false);
-		public static void StopPosixLowMemThread() { stopPosixLowMemThreadEvent.Set(); }
+		private static readonly ManualResetEvent StopPosixLowMemThreadEvent = new ManualResetEvent(false);
+		private static readonly ManualResetEvent LowPosixMemorySimulationEvent = new ManualResetEvent(false);
+
+		public static void StopPosixLowMemThread()
+		{
+			StopPosixLowMemThreadEvent.Set();
+		}
 
 		public static readonly FixedSizeConcurrentQueue<LowMemoryCalledRecord> LowMemoryCallRecords = new FixedSizeConcurrentQueue<LowMemoryCalledRecord>(100);
 
+		static MemoryStatistics()
+		{
+			LowMemoryHandlers = new ConcurrentSet<WeakReference<ILowMemoryHandler>>();
 			if (EnvironmentUtils.RunningOnPosix)
 			{
 				MemoryStatisticsForPosix();
@@ -65,20 +72,23 @@ namespace Raven.Database.Config
 			}
 
 			LowMemorySimulationEvent = CreateEvent(IntPtr.Zero, false, false, null);
-			lowMemoryNotificationHandle = CreateMemoryResourceNotification(LowMemoryResourceNotification); // the handle will be closed by the system if the process terminates
+			LowMemoryNotificationHandle = CreateMemoryResourceNotification(LowMemoryResourceNotification); // the handle will be closed by the system if the process terminates
 
+			var appDomainUnloadEvent = CreateEvent(IntPtr.Zero, true, false, null);
+			AppDomain.CurrentDomain.DomainUnload += (sender, args) => SetEvent(appDomainUnloadEvent);
 
-            if (LowMemoryNotificationHandle == null)
-				throw new Win32Exception ();
+			if (LowMemoryNotificationHandle == null)
+				throw new Win32Exception();
 
-			new Thread (() => {
-                const uint WAIT_FAILED = 0xFFFFFFFF;
-                const uint WAIT_TIMEOUT = 0x00000102;
-                while (true)
-                {
+			new Thread(() =>
+			{
+				const uint WAIT_FAILED = 0xFFFFFFFF;
+				const uint WAIT_TIMEOUT = 0x00000102;
+				while (true)
+				{
 					var waitForResult = WaitForMultipleObjects(4,
-						new[] { LowMemoryNotificationHandle, appDomainUnloadEvent, LowMemorySimulationEvent, SoftMemoryReleaseEvent }, false,
-						5 * 60 * 1000);
+						new[] {LowMemoryNotificationHandle, appDomainUnloadEvent, LowMemorySimulationEvent, SoftMemoryReleaseEvent}, false,
+						5*60*1000);
 
 					handleWaitResults:
 					switch (waitForResult)
@@ -90,8 +100,8 @@ namespace Raven.Database.Config
 							// prevent triggering the event too frequent when the low memory notification object 
 							// is in the signaled state
 							waitForResult = WaitForMultipleObjects(2,
-								new[] { appDomainUnloadEvent, LowMemorySimulationEvent }, false,
-								60 * 1000);
+								new[] {appDomainUnloadEvent, LowMemorySimulationEvent}, false,
+								60*1000);
 							goto handleWaitResults;
 						case 1:
 							// app domain unload
@@ -101,7 +111,7 @@ namespace Raven.Database.Config
 
 							RunLowMemoryHandlers("Simulated low memory");
 							break;
-						case 3://SoftMemoryReleaseEvent
+						case 3: //SoftMemoryReleaseEvent
 							Log.Warn("Releasing memory before Garbage Collection operation");
 							RunLowMemoryHandlers("Soft memory release");
 							break;
@@ -109,7 +119,7 @@ namespace Raven.Database.Config
 							ClearInactiveHandlers();
 							break;
 						case WAIT_FAILED:
-							log.Warn("Failure when trying to wait for low memory notification. No low memory notifications will be raised.");
+							Log.Warn("Failure when trying to wait for low memory notification. No low memory notifications will be raised.");
 							break;
 					}
 					Thread.Sleep(TimeSpan.FromSeconds(60)); // prevent triggering the event to frequent when the low memory notification object is in the signaled state
@@ -129,20 +139,20 @@ namespace Raven.Database.Config
 				while (true)
 				{
 					int waitRC = // poll each 5 seconds
-						WaitHandle.WaitAny(new[] { stopPosixLowMemThreadEvent, lowPosixMemorySimulationEvent }, TimeSpan.FromSeconds(5));
+						WaitHandle.WaitAny(new[] {StopPosixLowMemThreadEvent, LowPosixMemorySimulationEvent}, TimeSpan.FromSeconds(5));
 					switch (waitRC)
 					{
 						case 0: // stopLowMemThreadEvent
-							log.Debug("MemoryStatisticsForPosix : stopLowMemThreadEvent triggered");
+							Log.Debug("MemoryStatisticsForPosix : stopLowMemThreadEvent triggered");
 							return;
 						case 1: // lowMemorySimulationEvent
-							lowPosixMemorySimulationEvent.Reset();
-							log.Warn("Low memory simulation, will try to reduce memory usage...");
+							LowPosixMemorySimulationEvent.Reset();
+							Log.Warn("Low memory simulation, will try to reduce memory usage...");
 							RunLowMemoryHandlers("System detected low memory");
 							break;
 						case WaitHandle.WaitTimeout: // poll available mem
 
-							lowPosixMemorySimulationEvent.Reset();
+							LowPosixMemorySimulationEvent.Reset();
 							if (++clearInactiveHandlersCounter > 60) // 5 minutes == WaitAny 5 Secs * 60
 							{
 								clearInactiveHandlersCounter = 0;
@@ -152,15 +162,16 @@ namespace Raven.Database.Config
 							sysinfo_t info = new sysinfo_t();
 							if (Syscall.sysinfo(ref info) != 0)
 							{
-								log.Warn("Failure when trying to wait for low memory notification. No low memory notifications will be raised.");
+								Log.Warn("Failure when trying to wait for low memory notification. No low memory notifications will be raised.");
 							}
 							else
 							{
-								ulong availableMem = info.AvailableRam / (1024L * 1024);
-								if (availableMem < (ulong)configuration.LowMemoryForLinuxDetectionInMB)
+								RavenConfiguration configuration = new RavenConfiguration();
+								ulong availableMem = info.AvailableRam/(1024L*1024);
+								if (availableMem < (ulong) configuration.LowMemoryForLinuxDetectionInMB)
 								{
 									clearInactiveHandlersCounter = 0;
-									log.Warn("Low memory detected, will try to reduce memory usage...");
+									Log.Warn("Low memory detected, will try to reduce memory usage...");
 									RunLowMemoryHandlers("System detected low memory manually");
 									Thread.Sleep(TimeSpan.FromSeconds(60)); // prevent triggering the event to frequent when the low memory notification object is in the signaled state
 								}
@@ -177,11 +188,11 @@ namespace Raven.Database.Config
 
 
 		public static void SimulateLowMemoryNotification()
-        {
+		{
 			if (EnvironmentUtils.RunningOnPosix == false)
 				SetEvent(LowMemorySimulationEvent);
 			else
-				lowPosixMemorySimulationEvent.Set();
+				LowPosixMemorySimulationEvent.Set();
 		}
 
 		public static void InitiateSoftMemoryRelease()
@@ -208,12 +219,12 @@ namespace Raven.Database.Config
 					try
 					{
 						var res = handler.HandleLowMemory();
-						if(!string.IsNullOrEmpty(res.Summary))
+						if (!string.IsNullOrEmpty(res.Summary))
 							stats.Operations.Add(res);
 					}
 					catch (Exception e)
 					{
-						log.Error("Failure to process low memory notification (low memory handler - " + handler + ")", e);
+						Log.Error("Failure to process low memory notification (low memory handler - " + handler + ")", e);
 					}
 				}
 				else
@@ -291,38 +302,38 @@ namespace Raven.Database.Config
 		}
 
 		public static void RegisterLowMemoryHandler(ILowMemoryHandler handler)
-        {
-            LowMemoryHandlers.Add(new WeakReference<ILowMemoryHandler>(handler));
-        }
+		{
+			LowMemoryHandlers.Add(new WeakReference<ILowMemoryHandler>(handler));
+		}
 
-        /// <summary>
-        ///  This value is in MB
-        /// </summary>
-        public static int TotalPhysicalMemory
-        {
-            get
-            {
-                if (failedToGetTotalPhysicalMemory)
-                    return 1024;
-                try
-                {
-				if (Type.GetType("Mono.Runtime") != null)
+		/// <summary>
+		///  This value is in MB
+		/// </summary>
+		public static int TotalPhysicalMemory
+		{
+			get
+			{
+				if (failedToGetTotalPhysicalMemory)
+					return 1024;
+				try
 				{
-					var pc = new PerformanceCounter("Mono Memory", "Total Physical Memory");
-					var totalPhysicalMemoryMegabytes = (int)(pc.RawValue / 1024 / 1024);
-					if (totalPhysicalMemoryMegabytes == 0)
-						totalPhysicalMemoryMegabytes = 128; // 128MB, the Mono runtime default
-					return totalPhysicalMemoryMegabytes;
-				}
+					if (Type.GetType("Mono.Runtime") != null)
+					{
+						var pc = new PerformanceCounter("Mono Memory", "Total Physical Memory");
+						var totalPhysicalMemoryMegabytes = (int) (pc.RawValue/1024/1024);
+						if (totalPhysicalMemoryMegabytes == 0)
+							totalPhysicalMemoryMegabytes = 128; // 128MB, the Mono runtime default
+						return totalPhysicalMemoryMegabytes;
+					}
 
-                    return (int)(new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory / 1024 / 1024);
-                }
-                catch(Exception e)
-                {
-                    Logger.ErrorException("Could not get the total amount of memory, will lie and say we have only 1GB", e);
-                    failedToGetTotalPhysicalMemory = true;
-                    return 1024;
-                }
+					return (int) (new Microsoft.VisualBasic.Devices.ComputerInfo().TotalPhysicalMemory/1024/1024);
+				}
+				catch (Exception e)
+				{
+					Logger.ErrorException("Could not get the total amount of memory, will lie and say we have only 1GB", e);
+					failedToGetTotalPhysicalMemory = true;
+					return 1024;
+				}
 			}
 		}
 
@@ -334,7 +345,7 @@ namespace Raven.Database.Config
 			{
 				if (MaxParallelismSet == false)
 				{
-					return (Environment.ProcessorCount * 2);
+					return (Environment.ProcessorCount*2);
 				}
 				return maxParallelism;
 			}
@@ -363,71 +374,71 @@ namespace Raven.Database.Config
 			}
 		}
 
-        private static readonly ILog Logger = LogManager.GetCurrentClassLogger();
+		private static readonly ILog Logger = LogManager.GetCurrentClassLogger();
 
-        public static int AvailableMemoryInMb
-        {
-            get
-            {
-                if (failedToGetAvailablePhysicalMemory)
-                {
-                    Logger.Info("Because of a previous error in getting available memory, we are now lying and saying we have 256MB free");
-                    return 256;
-                }
+		public static int AvailableMemoryInMb
+		{
+			get
+			{
+				if (failedToGetAvailablePhysicalMemory)
+				{
+					Logger.Info("Because of a previous error in getting available memory, we are now lying and saying we have 256MB free");
+					return 256;
+				}
 
-                try
-                {
-                    if (StorageEnvironmentOptions.RunningOnPosix)
-                    {
-                        using (TextReader reader = File.OpenText("/proc/meminfo"))
-                        {
-                            var match = Regex.Match(reader.ReadToEnd(), @"MemFree:\s*(\d+) kB");
-                            if (match.Success)
-                            {
-                                if (memoryLimitSet)
-                                    return Math.Min(MemoryLimit, Convert.ToInt32(match.Groups[1].Value) / 1024);
-                                return Convert.ToInt32(match.Groups[1].Value) / 1024;
-                            }
-                        }
-                    }
+				try
+				{
+					if (StorageEnvironmentOptions.RunningOnPosix)
+					{
+						using (TextReader reader = File.OpenText("/proc/meminfo"))
+						{
+							var match = Regex.Match(reader.ReadToEnd(), @"MemFree:\s*(\d+) kB");
+							if (match.Success)
+							{
+								if (memoryLimitSet)
+									return Math.Min(MemoryLimit, Convert.ToInt32(match.Groups[1].Value)/1024);
+								return Convert.ToInt32(match.Groups[1].Value)/1024;
+							}
+						}
+					}
 
-                    // The CLR Memory (CLR) = Live Object (LO) + Dead Objects (DO)
-                    // The Working Set (WS) = CLR + Live Unmanaged (LU) = LO + DO + LU
-                       
-                    // Used Memory (UM) = WS - DO = CLR + LU - DO = (LO + DO) + LU - DO = LO + LU
-                    // Available Memory (AM) = Total Memory (TM) - UM  = TM - ( LO + LU ) = TM - LO - LU
-                                        
-                    long alreadyAvailableMemory = (long)new Microsoft.VisualBasic.Devices.ComputerInfo().AvailablePhysicalMemory;
+					// The CLR Memory (CLR) = Live Object (LO) + Dead Objects (DO)
+					// The Working Set (WS) = CLR + Live Unmanaged (LU) = LO + DO + LU
 
-                    long workingSet = Process.GetCurrentProcess().WorkingSet64;
-                    long liveObjectMemory = GC.GetTotalMemory(false);                                                            
-                    
-                    // There is still no way for us to query the amount of unmanaged memory in the working set
-                    // so we will have to live with the over-estimation of the total available memory. 
+					// Used Memory (UM) = WS - DO = CLR + LU - DO = (LO + DO) + LU - DO = LO + LU
+					// Available Memory (AM) = Total Memory (TM) - UM  = TM - ( LO + LU ) = TM - LO - LU
+
+					long alreadyAvailableMemory = (long) new Microsoft.VisualBasic.Devices.ComputerInfo().AvailablePhysicalMemory;
+
+					long workingSet = Process.GetCurrentProcess().WorkingSet64;
+					long liveObjectMemory = GC.GetTotalMemory(false);
+
+					// There is still no way for us to query the amount of unmanaged memory in the working set
+					// so we will have to live with the over-estimation of the total available memory. 
 					// to compensate for that, we will already remove 20% of the live object used as the size
 					// of unmanaged memory we use
-                    long availableMemory = alreadyAvailableMemory + (workingSet - liveObjectMemory - ((int)(liveObjectMemory * 0.2)));
-                    int availablePhysicalMemoryInMb = (int)(availableMemory / 1024 / 1024);       
+					long availableMemory = alreadyAvailableMemory + (workingSet - liveObjectMemory - ((int) (liveObjectMemory*0.2)));
+					int availablePhysicalMemoryInMb = (int) (availableMemory/1024/1024);
 
-                    if (Environment.Is64BitProcess)
-                    {                    
-                        return memoryLimitSet ? Math.Min(MemoryLimit, availablePhysicalMemoryInMb) : availablePhysicalMemoryInMb;
-                    }
+					if (Environment.Is64BitProcess)
+					{
+						return memoryLimitSet ? Math.Min(MemoryLimit, availablePhysicalMemoryInMb) : availablePhysicalMemoryInMb;
+					}
 
-                    // we are in 32 bits mode, but the _system_ may have more than 4 GB available
-                    // so we have to check the _address space_ as well as the available memory
-                    // 32bit processes are limited to 1.5GB of heap memory
-                    int workingSetMb = (int)(Process.GetCurrentProcess().WorkingSet64 / 1024 / 1024);
-                    return memoryLimitSet ? Math.Min(MemoryLimit, Math.Min(1536 - workingSetMb, availablePhysicalMemoryInMb)) : Math.Min(1536 - workingSetMb, availablePhysicalMemoryInMb);
-                }
-                catch (Exception e)
-                {
-                    Logger.ErrorException("Error while trying to get available memory, will stop trying and report that there is 256MB free only from now on", e);
-                    failedToGetAvailablePhysicalMemory = true;
-                    return 256;
-                }
-            }
-        }
-
+					// we are in 32 bits mode, but the _system_ may have more than 4 GB available
+					// so we have to check the _address space_ as well as the available memory
+					// 32bit processes are limited to 1.5GB of heap memory
+					int workingSetMb = (int) (Process.GetCurrentProcess().WorkingSet64/1024/1024);
+					return memoryLimitSet ? Math.Min(MemoryLimit, Math.Min(1536 - workingSetMb, availablePhysicalMemoryInMb)) : Math.Min(1536 - workingSetMb, availablePhysicalMemoryInMb);
+				}
+				catch (Exception e)
+				{
+					Logger.ErrorException("Error while trying to get available memory, will stop trying and report that there is 256MB free only from now on", e);
+					failedToGetAvailablePhysicalMemory = true;
+					return 256;
+				}
+			}
 		}
 	}
+}
+
